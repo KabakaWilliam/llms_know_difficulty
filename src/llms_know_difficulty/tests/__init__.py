2e7d5a49882dc94abc7610ab32d9a6619460fbac
williambankes <williamjamesbankes@gmail.com>
2026-01-09 17:31:48 +0100

    added test dir for attn probe tests

---
+++ src/llms_know_difficulty/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for llms_know_difficulty package."""
---
+++ src/llms_know_difficulty/tests/run_attn_probe_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run the sklearn probe test from the tests directory
+set -e
+
+# Get the project src directory (parent of parent of tests)
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+# Run the test module
+cd "$SRC_DIR"
+export CUDA_VISIBLE_DEVICES=0
+python -m llms_know_difficulty.tests.test_attn_probe_real_data "$@"
+# python -m llms_know_difficulty.tests.test_sklearn_probe_e2e "$@"
