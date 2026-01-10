3e2259bbeecc6e8c0b2b4ed1338ceed01c6f73f0
williambankes <williamjamesbankes@gmail.com>
2026-01-10 18:28:18 +0100

    updates to main.py

--- src/llms_know_difficulty/main.py
+++ src/llms_know_difficulty/main.py
@@ -1,8 +1,10 @@
 import argparse
-from config import DATASETS
-from utils import create_results_path
 from probe.probe_factory import ProbeFactory
-
+from utils import (
+    create_results_path,
+    DataIngestionWorkflow,
+    save_probe_predictions
+    )
 
 def main():
     parser = argparse.ArgumentParser(description="LLMs-Know-Difficulty command line interface")
@@ -10,41 +12,55 @@ def main():
     parser.add_argument("--dataset", type=str, required=False, help="Path to data file")
     parser.add_argument("--model", type=str, required=False, help="Name of model to use")
     parser.add_argument("--checkpoint_path", type=str, required=False, help="Path to checkpoint file")
+    parser.add_argument("--max_len", type=int, required=False, help="Maximum length of the response")
+    parser.add_argument("--k", type=int, required=False, help="Number of rollouts per question")
+    parser.add_argument("--temperature", type=float, required=False, help="Temperature for the model")
     args = parser.parse_args()
 
     print("Args:", args)
 
-    assert args.dataset in DATASETS, f"Dataset {args.dataset} not found in {DATASETS}"
-
     # 1. Load the dataset from the config
-    # TODO: Talk to Thom about what this looks like...
+    train_data, val_data, test_data = DataIngestionWorkflow.load_dataset(
+        dataset_name=args.dataset,
+        model_name=args.model,
+        max_len=args.max_len,
+        k=args.k,
+        temperature=args.temperature)
     
-
-    # 2. Setup the various metrics we're using 
-    # TODO: Discuss how we pass this to probes ...
-
+    # 2. Setup the various metrics we're using -> TODO
 
     # 3. Setup the results directory for the run 
     results_path = create_results_path(args.dataset, args.model, args.probe)
     print(f"Creating results directory at {results_path}")
 
     # 4. Initialize the probe:
-    probe, setup_args = ProbeFactory.create_probe(probe_name=args.probe)
-
-    # 5. Run probe setup:
-    probe.setup(**setup_args)
+    print(f"Initializing probe {args.probe}")
+    probe = ProbeFactory.create_probe(probe_name=args.probe, 
+                                        model=args.model,
+                                        dataset=args.dataset,
+                                        max_len=args.max_len,
+                                        k=args.k,
+                                        temperature=args.temperature)
 
     if args.checkpoint_path is not None:
+        print(f"Loading probe from checkpoint {args.checkpoint_path}")
         probe.init_model(checkpoint_path=args.checkpoint_path)
 
     # 6. Run probe training:
-    probe.train(train_data, val_data)
+    print(f"Training probe on train and val data")
+    probe = probe.train(train_data=train_data, val_data=val_data)
 
     # 7. Run probe prediction on the test set:
-    probe_preds = probe.predict(test_data)
+    print(f"Predicting on test data")
+    probe_preds = probe.predict(prompts=test_data[0])
 
     # 8. Save the probe predictions to the results directory:
-    save_probe_predictions(probe_preds, results_path)
+    results_path = create_results_path(args.dataset, args.model, args.probe)
+    print(f"Saving probe predictions to {results_path}")
+    save_probe_predictions(probe_preds=probe_preds,
+     probe_metadata=probe.get_metadata(),
+     best_probe=probe,
+     results_path=results_path)
 
 
 if __name__ == "__main__":
