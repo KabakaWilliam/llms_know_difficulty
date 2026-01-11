ec763bc04ef150372050f9aa71fc9b444e06e71e
KabakaWilliam <ichigo.gitta@gmail.com>
2026-01-11 17:43:31 +0000

    added memory conservation flag for attention probe

--- run_src.bash
+++ run_src.bash
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python3 src/llms_know_difficulty/main.py --probe linear_eoi_probe --dataset DigitalLearningGmbH_MATH-lighteval --model Qwen/Qwen2.5-Math-1.5B-Instruct --max_len 3000 --k 8 --temperature 0.7
+CUDA_VISIBLE_DEVICES=0 python3 src/llms_know_difficulty/main.py --probe attn_probe --dataset DigitalLearningGmbH_MATH-lighteval --model Qwen/Qwen2.5-Math-1.5B-Instruct --max_len 3000 --k 8 --temperature 0.7
 
-# attn_probe
 # linear_eoi_probe
+# attn_probe ==> (PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True)
